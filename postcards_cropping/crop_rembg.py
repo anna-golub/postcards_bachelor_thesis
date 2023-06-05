fb729255bca0fbeaf1517d6c3e12f235e96ea2e2
anna-golub <anna.golub.923@gmail.com>
2023-06-05 21:05:39 +0200

    remove old files

--- postcards_cropping/crop_rembg.py
+++
@@ -1,9 +0,0 @@
-from rembg import remove
-
-
-def crop_rembg(input_path, output_path):
-    with open(input_path, 'rb') as i:
-        with open(output_path, 'wb') as o:
-            input = i.read()
-            output = remove(input)
-            o.write(output)
